e13fc6e44cdc6bded453697711a85b9744855169
MalshanRuchira <malshanruchira20021220@gmail.com>
2024-09-08 12:47:19 +0530

    CRUD operation UI design
    UI design of the main ui with crud operations

---
+++ Form2.Designer.cs
@@ -0,0 +1,487 @@
+﻿namespace FinalProject
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            linkLabel1 = new LinkLabel();
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            button3 = new Button();
+            button4 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            groupBox4 = new GroupBox();
+            textBox9 = new TextBox();
+            label13 = new Label();
+            textBox8 = new TextBox();
+            label12 = new Label();
+            textBox7 = new TextBox();
+            label11 = new Label();
+            groupBox3 = new GroupBox();
+            textBox6 = new TextBox();
+            label10 = new Label();
+            textBox5 = new TextBox();
+            label9 = new Label();
+            textBox4 = new TextBox();
+            label8 = new Label();
+            textBox3 = new TextBox();
+            label7 = new Label();
+            groupBox2 = new GroupBox();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            label6 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label5 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            groupBox1.SuspendLayout();
+            groupBox4.SuspendLayout();
+            groupBox3.SuspendLayout();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.LinkColor = Color.White;
+            linkLabel1.Location = new Point(9, 9);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(45, 15);
+            linkLabel1.TabIndex = 0;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Logout";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(91, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(207, 30);
+            label1.TabIndex = 3;
+            label1.Text = "Skills Internationals";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(groupBox4);
+            groupBox1.Controls.Add(groupBox3);
+            groupBox1.Controls.Add(groupBox2);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.ForeColor = SystemColors.ButtonFace;
+            groupBox1.Location = new Point(22, 62);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(330, 576);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Student Registration";
+            // 
+            // button3
+            // 
+            button3.ForeColor = SystemColors.ActiveCaptionText;
+            button3.Location = new Point(187, 540);
+            button3.Name = "button3";
+            button3.Size = new Size(62, 23);
+            button3.TabIndex = 9;
+            button3.Text = "Clear";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click_1;
+            // 
+            // button4
+            // 
+            button4.ForeColor = SystemColors.ActiveCaptionText;
+            button4.Location = new Point(255, 540);
+            button4.Name = "button4";
+            button4.Size = new Size(69, 23);
+            button4.TabIndex = 8;
+            button4.Text = "Delete";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button2
+            // 
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(78, 540);
+            button2.Name = "button2";
+            button2.Size = new Size(64, 23);
+            button2.TabIndex = 6;
+            button2.Text = "Update";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Location = new Point(6, 540);
+            button1.Name = "button1";
+            button1.Size = new Size(66, 23);
+            button1.TabIndex = 5;
+            button1.Text = "Register";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(textBox9);
+            groupBox4.Controls.Add(label13);
+            groupBox4.Controls.Add(textBox8);
+            groupBox4.Controls.Add(label12);
+            groupBox4.Controls.Add(textBox7);
+            groupBox4.Controls.Add(label11);
+            groupBox4.ForeColor = SystemColors.ButtonHighlight;
+            groupBox4.Location = new Point(31, 401);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(273, 133);
+            groupBox4.TabIndex = 4;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Parent Details";
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(94, 90);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(171, 23);
+            textBox9.TabIndex = 20;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(14, 93);
+            label13.Name = "label13";
+            label13.Size = new Size(68, 15);
+            label13.TabIndex = 19;
+            label13.Text = "Contact No";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(94, 56);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(171, 23);
+            textBox8.TabIndex = 18;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(14, 64);
+            label12.Name = "label12";
+            label12.Size = new Size(27, 15);
+            label12.TabIndex = 17;
+            label12.Text = "NIC";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(94, 26);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(171, 23);
+            textBox7.TabIndex = 16;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(12, 29);
+            label11.Name = "label11";
+            label11.Size = new Size(76, 15);
+            label11.TabIndex = 0;
+            label11.Text = "Parent Name";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(textBox6);
+            groupBox3.Controls.Add(label10);
+            groupBox3.Controls.Add(textBox5);
+            groupBox3.Controls.Add(label9);
+            groupBox3.Controls.Add(textBox4);
+            groupBox3.Controls.Add(label8);
+            groupBox3.Controls.Add(textBox3);
+            groupBox3.Controls.Add(label7);
+            groupBox3.ForeColor = SystemColors.ButtonHighlight;
+            groupBox3.Location = new Point(31, 239);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(273, 156);
+            groupBox3.TabIndex = 3;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Contact Details";
+            groupBox3.Enter += groupBox3_Enter;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(96, 117);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(171, 23);
+            textBox6.TabIndex = 15;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(14, 119);
+            label10.Name = "label10";
+            label10.Size = new Size(59, 15);
+            label10.TabIndex = 14;
+            label10.Text = "Home No";
+            label10.Click += label10_Click;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(96, 88);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(171, 23);
+            textBox5.TabIndex = 13;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(14, 91);
+            label9.Name = "label9";
+            label9.Size = new Size(63, 15);
+            label9.TabIndex = 12;
+            label9.Text = "Mobile No";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(96, 57);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(171, 23);
+            textBox4.TabIndex = 11;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(14, 65);
+            label8.Name = "label8";
+            label8.Size = new Size(36, 15);
+            label8.TabIndex = 10;
+            label8.Text = "Email";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(96, 22);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(171, 23);
+            textBox3.TabIndex = 9;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(12, 28);
+            label7.Name = "label7";
+            label7.Size = new Size(49, 15);
+            label7.TabIndex = 0;
+            label7.Text = "Address";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(radioButton2);
+            groupBox2.Controls.Add(radioButton1);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(dateTimePicker1);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(textBox2);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(textBox1);
+            groupBox2.Controls.Add(label3);
+            groupBox2.ForeColor = SystemColors.ButtonHighlight;
+            groupBox2.Location = new Point(31, 67);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(274, 166);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Basic Details";
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(205, 130);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(63, 19);
+            radioButton2.TabIndex = 8;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Female";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(97, 132);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(51, 19);
+            radioButton1.TabIndex = 7;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Male";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(14, 132);
+            label6.Name = "label6";
+            label6.Size = new Size(45, 15);
+            label6.TabIndex = 6;
+            label6.Text = "Gender";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(97, 87);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(171, 23);
+            dateTimePicker1.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(14, 93);
+            label5.Name = "label5";
+            label5.Size = new Size(75, 15);
+            label5.TabIndex = 4;
+            label5.Text = "Date Of Birth";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(97, 55);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(171, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(14, 58);
+            label4.Name = "label4";
+            label4.Size = new Size(63, 15);
+            label4.TabIndex = 2;
+            label4.Text = "Last Name";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(97, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(171, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 27);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 15);
+            label3.TabIndex = 0;
+            label3.Text = "First Name";
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "1", "2", "3", "4", "5", "6" });
+            comboBox1.Location = new Point(107, 26);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(198, 23);
+            comboBox1.TabIndex = 1;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(34, 29);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Reg No";
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkSlateGray;
+            ClientSize = new Size(380, 650);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Controls.Add(linkLabel1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form2";
+            Text = "Student Registration - Skills International";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private LinkLabel linkLabel1;
+        private Label label1;
+        private GroupBox groupBox1;
+        private Label label2;
+        private GroupBox groupBox2;
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+        private Label label3;
+        private DateTimePicker dateTimePicker1;
+        private Label label5;
+        private TextBox textBox2;
+        private Label label4;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Label label6;
+        private GroupBox groupBox3;
+        private TextBox textBox4;
+        private Label label8;
+        private TextBox textBox3;
+        private Label label7;
+        private Label label10;
+        private TextBox textBox5;
+        private Label label9;
+        private GroupBox groupBox4;
+        private TextBox textBox6;
+        private Label label13;
+        private TextBox textBox8;
+        private Label label12;
+        private TextBox textBox7;
+        private Label label11;
+        private TextBox textBox9;
+        private Button button1;
+        private Button button4;
+        private Button button2;
+        private Button button3;
+    }
+}
